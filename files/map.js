d56c667eaf7064ab12952a82a0925cf682699a70
BalduinLandolt <balduin.landolt@unibas.ch>
2018-10-15 14:45:33 +0200

    tried to add json functionallity. not working so far

--- files/map.js
+++ files/map.js
@@ -94,6 +94,32 @@ function polyClick(x) {
 }
 
 
+/**
+ * loading Data from json file
+ */
+
+ function loadData(){
+    alert("Loading");
+    loadFromJson(function(response){});
+    var actual_JSON = JSON.parse(response);
+    alert("done loading");
+ }
+
+ function loadFromJson(callback){
+
+    var xobj = new XMLHttpRequest();
+        xobj.overrideMimeType("application/json");
+    xobj.open('GET', 'data.json', false);
+    xobj.onreadystatechange = function () {
+          if (xobj.readyState == 4 && xobj.status == "200") {
+            // Required use of an anonymous callback as .open will NOT return a value but simply returns undefined in asynchronous mode
+            callback(xobj.responseText);
+          }
+    };
+    xobj.send(null);
+ }
+
+
 /* polygons and data sets */
 //
 //
